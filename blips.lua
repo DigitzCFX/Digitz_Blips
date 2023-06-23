8e69faa89a5c1785425640dc9a092bf85ac0c281
Digitz <137466307+DigitzCFX@users.noreply.github.com>
2023-06-23 06:01:33 -0700

    Add files via upload

---
+++ blips.lua
@@ -0,0 +1,21 @@
+local blips = {
+
+    -- New Blip Template {title="", colour=, id=, x=, y=, z=},
+
+	 {title="The Diamond Casino", colour=57, id=617, x = 922.958, y = 48.02, z = 81.09},
+  }
+      
+Citizen.CreateThread(function()
+
+    for _, info in pairs(blips) do
+      info.blip = AddBlipForCoord(info.x, info.y, info.z)
+      SetBlipSprite(info.blip, info.id)
+      SetBlipDisplay(info.blip, 4)
+      SetBlipScale(info.blip, 1.0)
+      SetBlipColour(info.blip, info.colour)
+      SetBlipAsShortRange(info.blip, true)
+	  BeginTextCommandSetBlipName("STRING")
+      AddTextComponentString(info.title)
+      EndTextCommandSetBlipName(info.blip)
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Digitz'
+description 'Example resource'
+version 'v.01'
+
+client_script
+'blips.lua'
